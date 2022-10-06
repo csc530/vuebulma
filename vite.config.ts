247b75c595814778c91391cd55466e053f61e278
csc530 <{ID}+{username}@users.noreply.github.com>
2022-10-06 19:23:14 -0400

    :wrench: build(dist): Made an untested build for the created components
    Add config to vite for library mode that will hopefully be able to be imported to other projects and used normally

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path';
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [vue()],
+	css: {
+		preprocessorOptions: {
+			sass: {
+				// language=SASS
+				additionalData: `@import "node_modules/bulma/sass/utilities/_all.sass"\n` +
+					`@import "node_modules/bulma/sass/base/all"\n` +
+					`@import "node_modules/bulma/sass/helpers/all"`
+			}
+		}
+	},
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points
+			entry: resolve(__dirname, 'lib/main.js'),
+			name: 'VueBulma',
+			// the proper extensions will be added
+			fileName: 'vue-bulma',
+			formats: ['es', 'cjs', 'umd', 'iife']
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['vue'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: 'Vue'
+				}
+			}
+		}
+	}
+});
